3fbc9f3266d19d37ceaf101021b48bbb3158ed55
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2026-03-20 10:49:46 +0100

    update

--- app/public/js/nav.js
+++ app/public/js/nav.js
@@ -4,17 +4,37 @@ document.addEventListener('DOMContentLoaded', () => {
     const nav = document.getElementById('topbar');
     if (!nav) return;
     nav.innerHTML = `
-        <header class="topbar">
-            <div class="container">
-                <div class="brand">Alcohol.ch</div>
-                <nav class="menu">
-                    <a href="/">Accueil</a>
-                    <!--<a href="/profile">Profil</a>-->
-                    <!--<a href="/admin">Admin</a>-->
-                    <!--<a href="/login">Connexion</a>-->
-                    <!--<a href="/register">Inscription</a>-->
-                </nav>
+  <header class="topbar">
+            <div class="container header-content">
+                <div class="brand"><a href="/">Alcohol.ch</a></div>
+
+                <button class="catalog-btn">
+                    <span>☰</span> Tous les produits
+                </button>
+
+                <div class="search-bar">
+                    <input type="text" placeholder="Je cherche...">
+                    <button class="search-btn">🔍</button>
+                </div>
+
+                <div class="user-actions">
+                    
+                    <a href="/login" class="account-link">
+                        <span class="material-symbols-outlined">
+person
+</span></span> Se connecter
+                    </a>
+                </div>
             </div>
         </header>
+        
+        <nav class="sub-menu">
+            <div class="container">
+                <a href="/">Accueil</a>
+                <a href="/promotions">Promotions</a>
+                <a href="/admin">Administration</a>
+                <a href="/profile">Mon Profil</a>
+            </div>
+        </nav>
     `;
 });
